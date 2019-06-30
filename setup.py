f905c0c4edd22b400aed3a5a88c01bba16056e22
Adam Taylor <aptaylor@theiet.org>
2019-06-30 16:37:44 +0100

    update
    update for structure

--- setup.py
+++ setup.py
@@ -1,5 +1,44 @@
 from setuptools import setup, find_pakages
 import pynq_proj
+from distutils.dir_util import copy_tree
+import os
+import shutil
+
+# global variables
+board = os.environ['BOARD']
+repo_board_folder = f'boards/{board}/resizer'
+board_notebooks_dir = os.environ['PYNQ_JUPYTER_NOTEBOOKS']
+hw_data_files = []
+
+
+# check whether board is supported
+def check_env():
+    if not os.path.isdir(repo_board_folder):
+        raise ValueError("Board {} is not supported.".format(board))
+    if not os.path.isdir(board_notebooks_dir):
+        raise ValueError("Directory {} does not exist.".format(board_notebooks_dir))
+
+
+# copy overlays to python package
+def copy_overlays():
+    src_ol_dir = os.path.join(repo_board_folder, 'bitstream')
+    dst_ol_dir = os.path.join('neo_pixel', 'bitstream')
+    copy_tree(src_ol_dir, dst_ol_dir)
+    hw_data_files.extend([os.path.join("..", dst_ol_dir, f) for f in os.listdir(dst_ol_dir)])
+
+
+# copy notebooks to jupyter home
+def copy_notebooks():
+    src_nb_dir = os.path.join(repo_board_folder, 'notebooks')
+    dst_nb_dir = os.path.join(board_notebooks_dir, 'neopixel')
+    if os.path.exists(dst_nb_dir):
+        shutil.rmtree(dst_nb_dir)
+    copy_tree(src_nb_dir, dst_nb_dir)
+
+
+check_env()
+copy_overlays()
+copy_notebooks()
 
 setup(
 	name = "neo_pynq",
